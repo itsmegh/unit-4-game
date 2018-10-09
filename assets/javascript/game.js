4c9709cf5df8e8283069e3cccddc425f4cc4ba4a
itsmegh <42684917+itsmegh@users.noreply.github.com>
2018-10-09 13:39:58 -0600

    Added pseudo code to javascript file

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+//Click any button to start the game
+//When the game loads, there will be four crystals showing with random values
+    //values should be between 1-12
+//When the game loads, there will be a random target number 
+    //value should be between 19-120
+//When game loads - if not the first time, have counter update wins/losses from last round
+
+//Game Play
+//When player clicks on a crystal, the points will show up in crystal value total
+    //all crystal clicks will add together
+    //when the score exceeds or meets the target number, the player wins/losses
+//Game ends when a player hits the target number or goes above the target number
+
+//New game will reset the target number, crystal values, update win/loss numbers
+// crystal value will also reset to 0
+
